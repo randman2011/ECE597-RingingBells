1658f81c334452a2779bd6b9de44239592d66b86
MarkMorrison <morrisma@rose-hulman.edu>
2014-11-13 14:07:28 -0500

    edits from Tuesday

--- RingerScript.js
+++ RingerScript.js
@@ -3,25 +3,8 @@ var s = require("child_process");
 var cluster = new require("cluster");
 var frequency = 121 // 50 Hz
 
-var numWorkers = 3;
+var numWorkers = 2;
 var nextWorker = 0;
-
-var f1 = function SendI2C(e)
-{
-	console.log("SendI2C");
-	var params = e.split(" ");
-	var chipAddress = params[0];
-	var regAddress = params[1];
-	var data = params[2];
-	var raw = Math.floor(data * 386 / 180 + 145);
-	var rawH = Math.floor(raw / 256);
-	var rawL = raw % 256;
-	console.log(chipAddress + " " + regAddress + " " + data);
-	s.exec("i2cset -y 2 " + (0x40 + chipAddress) + " " + (6 + 4*regAddress) + " 0x00");
-	s.exec("i2cset -y 2 " + (0x40 + chipAddress) + " " + (7 + 4*regAddress) + " 0x00");
-	s.exec("i2cset -y 2 " + (0x40 + chipAddress) + " " + (8 + 4*regAddress) + " " + rawL);
-	s.exec("i2cset -y 2 " + (0x40 + chipAddress) + " " + (9 + 4*regAddress) + " " + rawH);
-}
 
 InitializeI2C();
 
@@ -32,25 +15,19 @@ if (cluster.isMaster)
 	{
 		cluster.fork();
 	}
-
-	var repetitions = 4;
+	var threads = new Array(numWorkers);
 	var index = 0;
+	for (var id in cluster.workers)
+	{
+		threads[index] = cluster.workers[id];
+		index++;
+	}
 	setInterval(function()
 	{
-		index = index%repetitions;
-		x = 0;
-		for (var id in cluster.workers) 
-		{
-  	  if (x==nextWorker)
-			{
-				cluster.workers[id].send(index + " 60 120 1000 30");
-			}
-			break;
-    }
-    nextWorker++;
-    nextWorker = nextWorker%numWorkers;
-    index++;
-	}, 500);
+		threads[nextWorker].send(nextWorker + " 75 105 10000 20");
+		nextWorker++;
+		nextWorker = nextWorker%numWorkers;
+    }, 5000);
 }
 else
 {
@@ -75,7 +52,8 @@ else
 					s.exec("i2cset -y 2 " + (0x40 + chipAddress) + " " + (8 + 4*regAddress) + " " + rawL);
 					s.exec("i2cset -y 2 " + (0x40 + chipAddress) + " " + (9 + 4*regAddress) + " " + rawH);
 				}, params[4]);
-			var s2 = setInterval(function()
+			var s2;
+			setTimeout(function() { s2 = setInterval(function()
 				{
 					//var params = e.split(" ");
 					var chipAddress = Math.floor(params[0]/16);
@@ -90,11 +68,12 @@ else
 					s.exec("i2cset -y 2 " + (0x40 + chipAddress) + " " + (8 + 4*regAddress) + " " + rawL);
 					s.exec("i2cset -y 2 " + (0x40 + chipAddress) + " " + (9 + 4*regAddress) + " " + rawH);
 					flag = true;
-				}, params[4]*2);
-			setTimeout(function() { 
+				}, params[4])
+				}, params[4]);
+			setTimeout(function() { setTimeout(function() { 
 				clearTimeout(s1);
 				clearTimeout(s2);
-			}, params[3]);
+			}, params[3])}, params[4]);
 		});
 //  }
 }
