a586974f83e069d923c6efd2f3045c7eb6f59aa5
Randy Turner <randallsturner13@gmail.com>
2014-11-13 20:21:01 +0000

    Begin converting function to c

---
+++ RingerScript.c
@@ -0,0 +1,25 @@
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "i2c-dev.h"
+#include "i2cbusses.h"
+#include <pthread.h>
+
+int numWorkers = 2;
+int nextWorker = 0;
+
+void main(int argc, char *argv[])
+{
+    pthread[numWorkers] threads;
+    for (int x = 0; x < numWorkers; x++)
+    {
+        pthread[x] = 
+    }
+}
+
+void ThreadFunction()
+{
+    
+}
