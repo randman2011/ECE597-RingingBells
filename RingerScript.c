b2299405bc50f7446bdc2727b720bc54c7a73425
MarkMorrison <morrisma@rose-hulman.edu>
2014-11-18 08:19:13 -0500

    working...

--- RingerScript.c
+++ RingerScript.c
@@ -11,9 +11,11 @@
 #include <fcntl.h>
 #include <poll.h>
 
-#define NUM_BELLS 6
-#define BELLS_PER 5
-#define ANGLE 30
+#define NUM_BELLS 4
+#define BELLS_PER 2
+#define ANGLE 30      //degrees peak-to-peak
+#define DELAY 15000   //microseconds
+#define SPEED_ADJUST 0  //1 - speed adjusts based on distance from sensor
 
 //int numWorkers = 2;
 //int nextWorker = 0;
@@ -23,9 +25,16 @@ void signal_handler(int sig);
 void *ThreadFunction(void *param);
 void SendI2C(int chipAddress, int bellNumber, int deg);
 void InitI2C(void);
+void InitAIN(void);
 
 #define POLL_TIMEOUT (3*1000)
 #define MAX_BUF 64
+
+typedef struct Thread{
+  pthread_t tid;
+  int id;
+  pthread_mutex_t mutex;
+} Thread;
 
 //signal handler function (to kill entire program cleanly)
 void signal_handler(int sig){
@@ -40,16 +49,22 @@ void main(int argc, char *argv[])
   int timeout;
   char buf[MAX_BUF];
   int i = 0;
-  pthread_t threads[NUM_BELLS];
+  pthread_mutex_t i2c_mutex;
+  Thread threads[NUM_BELLS];
   
   signal(SIGINT, signal_handler);
 
   timeout = POLL_TIMEOUT;
   
   InitI2C();
+  InitAIN();
+  
+  pthread_mutex_init(&i2c_mutex,NULL);
 
   for(i = 0; i < NUM_BELLS; i++){
-    pthread_create(&threads[i], NULL, ThreadFunction, &i);
+    threads[i].id = i;
+    threads[i].mutex = i2c_mutex;
+    pthread_create(&threads[i].tid, NULL, ThreadFunction, (threads+i));
   }
 
   while(keepgoing){
@@ -67,20 +82,21 @@ void main(int argc, char *argv[])
   
   i = 0;
   while(i < NUM_BELLS){
-    pthread_join(threads[i], NULL);
+    pthread_join(threads[i].tid, NULL);
     i++;
   }
-  
-  //for (int x = 0; x < numWorkers; x++){
-  //  pthread_create(&threads[x], NULL, ThreadFunction, NULL);
-  // }
 }
 
 void *ThreadFunction(void *param){
-  int ain = *(int*)param;
+  Thread *t = param;
+  int ain = t->id;
+  pthread_mutex_t i2c_mutex = t->mutex;
   int ain_value;
-  int chipAddress = ain * 2 / NUM_BELLS - 1;
+  int chipAddress = ain * 2 / NUM_BELLS;
+  int bell = (ain - chipAddress*NUM_BELLS/2) * BELLS_PER;
   int dir = 0, i;
+  
+  printf("Thread %d chipAddress %d ain %d\n", ain, chipAddress, ain);
   
   while(keepgoing){  //change this to some signal, sent by main program, to end
     ain_get_value(ain, &ain_value);
@@ -90,19 +106,30 @@ void *ThreadFunction(void *param){
       //ring
       
       if(dir){
-	//go one direction
-	for(i = 0; i < BELLS_PER; i++){
-	  SendI2C(chipAddress, i + ain * 5, 90-ANGLE/2); 
-	}
+	      //go one direction
+	      for(i = 0; i < BELLS_PER; i++){
+	        pthread_mutex_lock(&i2c_mutex);
+	        SendI2C(chipAddress, i + bell, 90-ANGLE/2); 
+	        //usleep(1000);
+	        pthread_mutex_unlock(&i2c_mutex);
+	      }
       }else{
-	//go other direction
-	for(i = 0; i < BELLS_PER; i++){
-	  SendI2C(chipAddress, i + ain * 5, 90+ANGLE/2); 
-	}
+	      //go other direction
+	      for(i = 0; i < BELLS_PER; i++){
+	        pthread_mutex_lock(&i2c_mutex);
+	        SendI2C(chipAddress, i + bell, 90+ANGLE/2);
+	        //usleep(1000);
+	        pthread_mutex_unlock(&i2c_mutex);
+	      }
       }
+      
       dir = (dir + 1) % 2;
     }
-    usleep(ain_value * 500);
+    if(SPEED_ADJUST){
+      usleep(ain_value * 500);
+    } else {
+      usleep(DELAY);
+    }
   }
 }
 
@@ -114,25 +141,42 @@ void SendI2C(int chipAddress, int bellNumber, int deg){
   
   sprintf(buffer, "i2cset -y 2 %d %d 0", 0x40 + chipAddress, 6 + bellNumber*4);
   system(buffer);
+  //printf("%s\n", buffer);
   sprintf(buffer, "i2cset -y 2 %d %d 0", 0x40 + chipAddress, 7 + bellNumber*4);
   system(buffer);
+  //printf("%s\n", buffer);
   sprintf(buffer, "i2cset -y 2 %d %d %d", 0x40 + chipAddress, 8 + bellNumber*4, rawL);
   system(buffer);
+  //printf("%s\n", buffer);
   sprintf(buffer, "i2cset -y 2 %d %d %d", 0x40 + chipAddress, 9 + bellNumber*4, rawH);
   system(buffer);
+  //printf("%s\n", buffer);
   //i2c_smbus_write_word_data(file, regAddress, raw);
 }
 
 void InitI2C(){
   //setup i2c
-  system("echo BB-I2C1 > /sys/devices/bone_capemgr.*/slots");
+  system("echo BB-I2C1 > /sys/devices/bone_capemgr.9/slots");
+  printf("echo BB-I2C1 > /sys/devices/bone_capemgr.9/slots\n");
+  //usleep(10000);
   //set mode to sleep
   system("i2cset -y 2 0x40 0x00 0x11");
+  printf("i2cset -y 2 0x40 0x00 0x11\n");
   system("i2cset -y 2 0x41 0x00 0x11");
+  printf("i2cset -y 2 0x41 0x00 0x11\n");
   //set frequency (to 50 Hz)
   system("i2cset -y 2 0x40 0xfe 121");
+  printf("i2cset -y 2 0x40 0xfe 121\n");
   system("i2cset -y 2 0x41 0xfe 121");
+  printf("i2cset -y 2 0x41 0xfe 121\n");
   //set mode to normal
   system("i2cset -y 2 0x40 0x00 0x01");
+  printf("i2cset -y 2 0x40 0x00 0x01\n");
   system("i2cset -y 2 0x41 0x00 0x01");
+  printf("i2cset -y 2 0x41 0x00 0x01\n");
 }
+
+void InitAIN(){
+  system("echo cape-bone-iio > /sys/devices/bone_capemgr.9/slots");
+}
+
